74be4f47256e396a364f3a639acf8b5c501b77f3
kasun_vimu <kasunvimu8@gmail.com>
2021-10-16 22:02:47 +0530

    presets and source maps added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log('Hii')
-console.log('Hii2')
+var obj1 = {
+    name: 'kasun',
+    age: 23
+}
+
+var obj2 = {
+    ...obj1,
+    height: '6ft'
+}
+
+console.log(obj1)
+console.log(obj2)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-var path = require('path');
+let path = require('path');
+let mode = "development";
+
+if (process.env.NODE_ENV === 'production') {
+    mode = "production";
+}
 
 module.exports = {
-    mode: "development",
-    devtool: false,
-    
+    mode: mode,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    },
+
+    devtool: "source-map",
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
